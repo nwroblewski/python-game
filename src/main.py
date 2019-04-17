b900cb21c752f64c073bdb27e7ad36d919e48c38
nwroblewski <nwroblewski@pop-os.localdomain>
2019-04-17 06:14:50 +0200

    Messing with PyGame just got more object oriented

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env/python3
-
+from src.Assets import settings
 import pygame
-import numpy as np
 
 pygame.init()
 bg_image = pygame.image.load("Assets/PixelArt.png")
@@ -10,20 +9,89 @@ window_height = 640
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Gameee")
 
-x = 50
-y = 500
-width = 32
-height = 64
-speed = 5
 
-jumping = False
-jump_count = 10
+class Player(object):
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.heigth = height
+        self.speed = 5
+        self.jumping = False
+        self.moving_right = False
+        self.moving_left = False
+        self.jump_count = 8
+        self.walk_count = 0
+
+        self.char = pygame.image.load(settings.SPRITES_PATH + 'standing.png')
+
+        self.walk_right = [pygame.image.load(settings.SPRITES_PATH + 'R1.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'R2.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'R3.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'R4.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'R5.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'R6.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'R7.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'R8.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'R9.png')]
+
+        self.walk_left = [pygame.image.load(settings.SPRITES_PATH + 'L1.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'L2.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'L3.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'L4.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'L5.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'L6.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'L7.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'L8.png'),
+            pygame.image.load(settings.SPRITES_PATH + 'L9.png')]
+
+    def draw(self, game_window):
+
+        self.walk_count = self.walk_count % 27
+
+        if player.moving_left:
+            game_window.blit(self.walk_left[self.walk_count // 3], (self.x, self.y))
+            self.walk_count += 1
+
+        elif player.moving_right:
+            game_window.blit(self.walk_right[self.walk_count // 3], (self.x, self.y))
+            self.walk_count += 1
+        else:
+            game_window.blit(self.char, (self.x, self.y))
+
+    def jump(self):
+
+        if self.jump_count >= -8:
+            direction = 1
+            if self.jump_count < 0:
+                direction = -1
+            self.y -= (self.jump_count ** 2) * 0.5 * direction
+            self.jump_count -= 1
+
+        else:
+            self.jumping = False
+            self.jump_count = 8
+
+    def move(self):
+        pass
+
+
+bg = pygame.image.load(settings.SPRITES_PATH + 'bg.jpg')
 
 clock = pygame.time.Clock()
-# pygame.mixer.music.load('Assets/muzyczka.wav')
 running = True
 
-# pygame.mixer.music.play(-1)
+
+def draw(player_obj):
+    window.fill((0, 0, 0))
+    window.blit(bg_image, (0, 0))
+    player_obj.draw(window)
+    pygame.display.update()
+
+
+player = Player(40, 580, 64, 64)
+
 while running:
 
     for event in pygame.event.get():
@@ -35,42 +103,28 @@ while running:
     if keys[pygame.K_ESCAPE]:
         running = False
 
-    if jumping:
-        print(y)
-        if jump_count >= -10:
-            direction = 1
-            if jump_count < 0:
-                direction = -1
-            y -= (jump_count ** 2) * 0.5 * direction
-            jump_count -= 1
-
-        else:
-            jumping = False
-            jump_count = 10
+    if player.jumping:
+        player.jump()
     else:
         if keys[pygame.K_SPACE]:
-            jumping = True
+            player.jumping = True
 
-        if keys[pygame.K_DOWN] and y < window_height - height - speed:
-            y += speed
+        if keys[pygame.K_DOWN] and y < window_height - player.height - player.speed:
+            player.y += player.speed
 
-    if keys[pygame.K_RIGHT] and x < window_width - width - speed:
-        x += speed
+    if keys[pygame.K_RIGHT] and player.x < window_width - player.width - player.speed:
+        player.x += player.speed
+        player.moving_right = True
+        player.moving_left = False
+    elif keys[pygame.K_LEFT] and player.x > player.speed:
+        player.x -= player.speed
+        player.moving_left = True
+        player.moving_right = False
+    else:
+        player.moving_right = False
+        player.moving_left = False
+        player.walk_count = 0
 
-    if keys[pygame.K_LEFT] and x > speed:
-        x -= speed
-
-
-
-    # print(y,y_speed)
-
-    window.fill((0, 0, 0))
-    window.blit(bg_image, (0, 0))
-
-    pygame.draw.rect(window, (255, 0, 0), (x, y, width, height))
-
-    pygame.display.update()
-
-    clock.tick(40)
+    draw(player)
+    clock.tick(60)
 pygame.quit()
-
