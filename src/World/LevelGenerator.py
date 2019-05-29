b6b4953146aa084eb2cb666114f2db79995dafc4
KipinskiPiotr <49042374+KipinskiPiotr@users.noreply.github.com>
2019-05-30 01:25:39 +0200

    small change

--- src/World/LevelGenerator.py
+++ src/World/LevelGenerator.py
@@ -1,25 +1,35 @@
 import pygame
 from src.Assets import settings
-from src.Entities.platform import Platform
+from src.Entities.platform import *
 
 
 class LevelGenerator:
-    def __init__(self, platforms, entities, level):
+    def __init__(self, platforms, entities):
+        self.platforms = platforms
+        self.entities = entities
         self.tiles = [pygame.image.load(settings.SPRITES_PATH + 'tile1.png'),
-                pygame.image.load(settings.SPRITES_PATH + 'tile2.png')]
+                pygame.image.load(settings.SPRITES_PATH + 'tile2.png'),
+                pygame.image.load(settings.SPRITES_PATH + 'tileN.png')]
 
         for ind, val in enumerate(self.tiles):
             rect = val.get_rect()
             if not (rect.width == settings.TILE_SIZE and rect.height == settings.TILE_SIZE):
                 self.tiles[ind] = pygame.transform.scale(self.tiles[ind], (settings.TILE_SIZE, settings.TILE_SIZE))
 
+    def load(self, level):
+        # delete platforms and entities that existed ?
         f = open(settings.LEVELS_PATH + str(level), "r")
         x = 0
         y = 0
         for line in f.readlines():
             for char in line:
                 if char == "1":
-                    Platform(self.tiles[1], (x, y), platforms, entities)
+                    Platform(self.tiles[0], (x, y), self.platforms, self.entities)
+                if char == "2":
+                    Platform(self.tiles[1], (x, y), self.platforms, self.entities)
+                if char == "N":
+                    NextLevelPlatform(self.tiles[2], (x, y), self.platforms, self.entities)
                 x += settings.TILE_SIZE
             x = 0
             y += settings.TILE_SIZE
+        f.close()
