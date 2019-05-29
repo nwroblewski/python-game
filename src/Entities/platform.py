34e255e327613467271aa355bf8612d0e10122d1
KipinskiPiotr <49042374+KipinskiPiotr@users.noreply.github.com>
2019-05-30 01:26:51 +0200

    Added new platform

--- src/Entities/platform.py
+++ src/Entities/platform.py
@@ -5,3 +5,7 @@ from src.Assets import settings
 class Platform(Entity):
     def __init__(self, image, pos, *groups, width=settings.TILE_SIZE, height=settings.TILE_SIZE):
         super().__init__(image, width, height, pos, *groups)
+
+class NextLevelPlatform(Platform):
+    def __init__(self, image, pos, *groups, width=settings.TILE_SIZE, height = settings.TILE_SIZE):
+        super().__init__(image, pos, *groups)
