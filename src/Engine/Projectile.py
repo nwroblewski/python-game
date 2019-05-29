41386b01824800a7c6fca72fe8a0511860b6c8cf
nwroblewski <nwroblewski@pop-os.localdomain>
2019-05-29 04:39:06 +0200

    Added projectile class

---
+++ src/Engine/Projectile.py
@@ -0,0 +1,9 @@
+class Projectile:
+
+    # TODO add fancy projectile animation here
+    def __init__(self, x, y, radius, direction):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.direction = direction
+
