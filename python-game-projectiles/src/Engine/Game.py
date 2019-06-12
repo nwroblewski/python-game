1a4590d52d3f31e33a8da297e1ea9628997fca4f
KipinskiPiotr <49042374+KipinskiPiotr@users.noreply.github.com>
2019-06-12 15:24:52 +0200

    Client-server almost working

--- python-game-projectiles/src/Engine/Game.py
+++ python-game-projectiles/src/Engine/Game.py
@@ -2,24 +2,24 @@ import pygame as pg
 from src.Assets import settings
 import pygame
 from pygame import *
+from src.Engine.Client import Client
 
 
 class Game:
-    def __init__(self, entities, collisionDetector, bg):
+    def __init__(self, player, entities, collisionDetector, bg, window):
         self.clock = pygame.time.Clock()
-        self.players = []
+        self.player = player
+        self.players = {}
         self.entities = entities
         self.bg = bg
         self.collisionDetector = collisionDetector
+        self.window = window
 
-    def new_game(self):
-        pass
+    def start_communication(self):
+        self.client = Client(self.player, self.players)
+        self.client.run()
 
-    def add_player(self, player):
-        self.players.append(player)
-        player.rect = player.image.get_rect(topleft=settings.STARTING_POS)
-
-    def run(self, window):
+    def run(self):
         while 1:
             for e in pygame.event.get():
                 if e.type == QUIT:
@@ -27,20 +27,26 @@ class Game:
                 if e.type == KEYDOWN and e.key == K_ESCAPE:
                     return
             self.entities.update()
-            for player in self.players:
-                player.update_relative_position(player.rect.right + self.entities.cam.x)
+            self.player.update_relative_position(self.player.rect.right + self.entities.cam.x)
             self.collisionDetector.update()
-            window.blit(self.bg, (0, 0))
-            self.update_projectiles(window)
-            self.entities.draw(window)
+            self.window.blit(self.bg, (0, 0))
+            self.update_projectiles()
+            self.entities.draw(self.window)
+            self.draw_players()
             pygame.display.update()
             self.clock.tick(settings.FPS)
             #print(str(self.player.rect) + ' ' + str(self.player.vel))
 
-    def update_projectiles(self, window):
-        for player in self.players:
-            for projectile in player.projectiles:
-                if abs(projectile.x - player.win_x) > 800:
-                    player.projectiles.pop(player.projectiles.index(projectile))
-                projectile.draw(window)
-                projectile.update()
+    def update_projectiles(self):
+        for projectile in self.player.projectiles:
+            if abs(projectile.x - self.player.win_x) > 800:
+                self.player.projectiles.pop(self.player.projectiles.index(projectile))
+            projectile.draw(self.window)
+            projectile.update()
+
+    def draw_players(self):
+        print("PLAYERS:")
+        for id, pos in self.players.items():
+            print(f"{id} : {pos}")
+            if(self.client.id != id):
+                self.window.blit(self.player.image, pos)
